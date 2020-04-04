ede5ee12dea18164a1b2b99bbe8ca2100562d202
Markus Felten <Markus.Felten@gmx.de>
2020-04-05 01:48:08 +0200

    fix(rollup): use untranspiled *.mjs config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,59 @@
-import builtins from "builtin-modules";
-
+import { readFileSync } from "fs";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import native from "rollup-plugin-native";
+import cleanup from "rollup-plugin-cleanup";
+import consts from "rollup-plugin-consts";
 import acornClassFields from "acorn-class-fields";
 
-import consts from "rollup-plugin-consts";
-import cleanup from "rollup-plugin-cleanup";
-import native from "rollup-plugin-native";
-import executable from "rollup-plugin-executable";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import { readFileSync } from "fs";
+import builtins from "builtin-modules";
+
+const { name, version, description, main, module, bin } = JSON.parse(
+  readFileSync("./package.json", { encoding: "utf8" })
+);
+
+const external = [...builtins, "bufferutil", "utf-8-validate"];
+
+const plugins = [
+  consts({
+    name,
+    version,
+    description,
+  }),
+  commonjs(),
+  resolve(),
+  native(),
+  cleanup({
+    extensions: ["js", "mjs"],
+  }),
+];
+
+const config = Object.keys(bin).map((name) => {
+  return {
+    input: `src/${name}-cli.mjs`,
+    output: {
+      plugins: [executable()],
+      file: bin[name],
+    },
+  };
+});
+
+if (module !== undefined && main !== undefined) {
+  config.push({
+    input: module,
+    output: {
+      file: main,
+    },
+  });
+}
+
+export default config.map((c) => {
+  c.output = {
+    interop: false,
+    externalLiveBindings: false,
+    format: "cjs",
+    ...c.output,
+  };
+  return { acornInjectPlugins: [acornClassFields], plugins, external, ...c };
+});
